d061f90f7583a964c4e22a393710096cab1f3dd8
Sergey Vartanov <me@enzet.ru>
2022-08-30 23:36:44 +0300

    Add new commands
      - `c` is for circle,
      - `r` is for removing, `a` is for adding,
      - `w` is for setting width.

--- main.js
+++ main.js
@@ -6,16 +6,22 @@
  * @see https://github.com/enzet/Roentgen
  */
 
-var scale = 4.2;
-var size = 16.0; // Icons are 14 × 14 pixels, and this is size of icon with
-                 // 1 pixel margin.
-var rows = 10;   // Number of rows (icons in a column).
-var columns = 6; // Number of columns (icons in a row).
+var scale = 4;
 
-var shift = new Point(0.5 * scale, 0.5 * scale);
+// Icons are 14 × 14 pixels, and this is size of icon with 1 pixel margin.
+var size = 20.0; 
+
+// Number of rows (icons in a column).
+var rows = 100; 
+
+// Number of columns (icons in a row).
+var columns = Math.floor(800 / scale / size);
+
+var shift = new Point(2.5 * scale, 2.5 * scale);
 var current = new Point(0, 0);
 
 var width = 1.0;
+var uniting = "add";
 
 var sketchOpacity = 0.2;
 var sketchColor = new Color(0, 0, 0);
@@ -50,11 +56,11 @@ function toCoordinates(position) {
  * @param {vector} position center position of a circle
  * @returns created circle
  */
-function addPoint(position) {
+function addPoint(position, radius) {
 
     return new Path.Circle({
         center: position,
-        radius: scale * width * 0.5,
+        radius: scale * width * 0.5 * radius,
         fillColor: sketchColor,
         opacity: sketchOpacity
     });
@@ -81,6 +87,19 @@ function addLine(from, to) {
     return path;
 }
 
+var shape = null;
+
+function combine(object) {
+    if (!shape) {
+        shape = new Path({fillColor: "blue", insert: false})
+    }
+    if (uniting == "add") {
+        shape = shape.unite(object, insert=false);
+    } else {
+        shape = shape.subtract(object, insert=false);
+    }
+}
+
 /**
  * Parse shape description file line by line.
  */
@@ -88,14 +107,13 @@ function parse() {
 
     var current = new Path.Circle([0, 0], 10);
 
-    shift = new Point(0.5 * scale, 0.5 * scale);
+    shift = new Point(2.5 * scale, 2.5 * scale);
 
     project.clear();
     addGrid();
 
     var lines = area.value.split("\n");
     var variables = {};
-    var shape = null;
 
     var lexemes = []
 
@@ -133,6 +151,10 @@ function parse() {
 
         if (lexeme == "}" && shape) {
 
+            if (fill) {
+                shape = shape.unite(fill);
+                fill = null;
+            }
             fakeShape = new Path({insert: true});
             fakeShape = fakeShape.unite(shape);
             fakeShape.translate([0, scale * size])
@@ -142,15 +164,16 @@ function parse() {
             shift += new Point(scale * size, 0);
             if (shift.x > scale * columns * size) {
                 shift += new Point(0, scale * size * 2);
-                shift.x = 0.5 * scale;
+                shift.x = 2.5 * scale;
             }
             shape = null;
-            continue;
-        }
+        } else if (lexeme == "l" || lexeme == "lf") {
+            // `L` means simple one pixel width line. `LF` means `L` but filled.
 
-        // `L` means simple one pixel width line. `LF` means `L` but filled.
-        if (lexeme == "l" || lexeme == "lf") {
-
+            if (fill) {
+                shape = shape.unite(fill);
+                fill = null;
+            }
             filled = (lexeme == "lf");
             fill = new Path({insert: true});
             fill.fillColor = sketchColor;
@@ -159,39 +182,39 @@ function parse() {
             var last = null;
 
             mode = "line";
-            continue;
-        }
-
-        if (lexeme == "p") {
-            mode = "point";
-            continue;
-        }
-
-        if (lexeme.includes(",")) {
+        } else if (mode == "width") {
+            width = Number(lexeme);
+            mode = null;
+        } else if (lexeme == "p") {
+            current = toCoordinates(lexemes[i + 1]);
+            i++;
+        } else if (lexeme == "r") {
+            uniting = "remove";
+        } else if (lexeme == "a") {
+            uniting = "add";
+        } else if (lexeme == "w") {
+            width = Number(lexemes[i + 1]);
+            i++;
+        } else if (lexeme == "c") {
+            center = toCoordinates(lexemes[i + 1]);
+            radius = Number(lexemes[i + 2]);
+            circle = addPoint(center, radius);
+            combine(circle);
+            i += 2;
+        } else if (lexeme.includes(",")) {
 
             coordinates = toCoordinates(lexeme);
 
-            if (mode == "point") {
-                current = coordinates;
-            }
-
             if (mode == "line") {
-                point = addPoint(coordinates);
+                point = addPoint(coordinates, 1);
 
-                if (!shape) {
-                    shape = new Path({fillColor: "blue", insert: false})
-                }
-                shape = shape.unite(point, insert=false);
-
+                combine(point);
                 if (filled) {
                     fill.add(coordinates);
                 }
                 if (last) {
                     segment = addLine(last, coordinates);
-                    shape = shape.unite(segment);
-                    if (filled) {
-                        shape = shape.unite(fill);
-                    }
+                    combine(segment);
                 }
                 last = coordinates;
             }
@@ -202,11 +225,11 @@ function parse() {
 /**
  * Draw grid part from (x1, y1) to (x2, y2).
  */
-function addGridLine(x1, y1, x2, y2) {
+function addGridLine(x1, y1, x2, y2, color, width, opacity) {
     var path = new Path();
-    path.strokeColor = "black";
-    path.strokeWidth = 0.2;
-    path.opacity = 0.4;
+    path.strokeColor = color;
+    path.strokeWidth = width;
+    path.opacity = opacity;
     path.add(new Point(x1, y1) * scale, new Point(x2, y2) * scale);
 }
 
@@ -214,11 +237,19 @@ function addGridLine(x1, y1, x2, y2) {
  * Draw rectangular grid for parsed icons.
  */
 function addGrid() {
-    for (var i = 0; i < size * rows; i++) {
-        addGridLine(0, i, size * columns, i);
+    // for (var i = 0; i < size * rows; i++) {
+    //     addGridLine(0, i, size * columns, i, 0.2, 0.2);
+    // }
+    // for (var i = 0; i < size * columns; i++) {
+    //     addGridLine(i, 0, i, size * rows, 0.2, 0.2);
+    // }
+
+    gray = new Color(0.9, 0.9, 0.9);
+    for (var i = 0; i <= size * rows; i += size) {
+        addGridLine(0, i, size * columns, i, gray, 6 * scale, 1);
     }
-    for (var i = 0; i < size * columns; i++) {
-        addGridLine(i, 0, i, size * rows);
+    for (var i = 0; i <= size * columns; i += size) {
+        addGridLine(i, 0, i, size * rows, gray, 6 * scale, 1);
     }
 }
 
