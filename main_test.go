d1c69d97cf44eb88560b6e63d9e134b4cfee2ec3
Sergey Vartanov <me@enzet.ru>
2022-09-06 00:07:29 +0300

    Swap expected and actual in assertions

--- main_test.go
+++ main_test.go
@@ -12,12 +12,12 @@ func CheckIcon(t *testing.T, command string) *Icon {
 
 	parsed, err := parse(antlr.NewInputStream(command))
 
-	assert.Equal(t, err, nil, "Error.")
-	assert.Equal(t, len(parsed), 1, "Unexpected number of icons.")
+	assert.Equal(t, nil, err, "Error.")
+	assert.Equal(t, 1, len(icons), "Unexpected number of icons.")
 
 	icon := parsed[0]
-	assert.Equal(t, icon.Name, "icon0")
-	assert.Equal(t, len(icon.Figures), 1, "Unexpected number of figures.")
+	assert.Equal(t, "icon0", icon.Name)
+	assert.Equal(t, 1, len(icon.Figures), "Unexpected number of figures.")
 
 	return icon
 }
@@ -26,36 +26,36 @@ func CheckIcon(t *testing.T, command string) *Icon {
 func TestLine(t *testing.T) {
 
 	icon := CheckIcon(t, "{ l 0,0 1,1 }")
-	assert.Equal(t, icon.Figures[0],
-		&Line{[]Position{{0, 0}, {1, 1}}, false, 1})
+	assert.Equal(t, &Line{[]Position{{0, 0}, {1, 1}}, false, 1},
+		icon.Figures[0])
 }
 
 // Test filled line parsing.
 func TestLineFilled(t *testing.T) {
 
 	icon := CheckIcon(t, "{ lf 0,0 1,1 }")
-	assert.Equal(t, icon.Figures[0],
-		&Line{[]Position{{0, 0}, {1, 1}}, true, 1})
+	assert.Equal(t, &Line{[]Position{{0, 0}, {1, 1}}, true, 1},
+		icon.Figures[0])
 }
 
 // Test arc parsing.
 func TestArc(t *testing.T) {
 
 	icon := CheckIcon(t, "{ ar 1,1 5 0.1 0.2 }")
-	assert.Equal(t, icon.Figures[0], &Arc{Position{1, 1}, 5, 0.1, 0.2, 1})
+	assert.Equal(t, &Arc{Position{1, 1}, 5, 0.1, 0.2, 1}, icon.Figures[0])
 }
 
 // Test circle parsing.
 func TestCircle(t *testing.T) {
 
 	icon := CheckIcon(t, "{ c 1,1 5 }")
-	assert.Equal(t, icon.Figures[0], &Circle{Position{1, 1}, 5, 1})
+	assert.Equal(t, &Circle{Position{1, 1}, 5, 1}, icon.Figures[0])
 }
 
 // Test rectangle parsing.
 func TestRectangle(t *testing.T) {
 
 	icon := CheckIcon(t, "{ s 1,1 2,2 }")
-	assert.Equal(t, icon.Figures[0],
-		&Rectangle{Position{1, 1}, Position{2, 2}, 1})
+	assert.Equal(t, &Rectangle{Position{1, 1}, Position{2, 2}, 1},
+		icon.Figures[0])
 }
